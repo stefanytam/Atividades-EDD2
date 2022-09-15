a6eb498d3d5fc1a6e679e116b0ee688069c4948b
stefanytam <97906503+stefanytam@users.noreply.github.com>
2022-09-15 14:46:52 -0300

    Add files via upload

---
+++ TP AGENDA/TP AGENDA/Contatos.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP_AGENDA
+{
+    class Contatos
+    {
+        private readonly List<Contato> agenda;
+
+        public Contatos(List<Contato> agenda)
+        {
+            this.agenda = agenda;
+        }
+
+        public bool adicionar(Contato c)
+        {
+            agenda.Add(c);
+            return true;
+        }
+        public Contato pesquisar(Contato c)
+        {
+            int posicao = agenda.IndexOf(c);
+            return agenda[posicao];
+        }
+
+        public bool alterar(Contato substituido, Contato c)
+        {
+            bool podeAlterar = false;
+            int posicao = agenda.IndexOf(substituido);
+            if (posicao != -1)
+            {
+                podeAlterar = true;
+                agenda[posicao] = c;
+            }
+            return podeAlterar;
+        }
+
+        public bool remover(Contato c)
+        {
+            return agenda.Remove(c);
+        }
+
+    }
+}
