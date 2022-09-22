36c2b11e1548b9b185fbe0cb7f9f14af67548306
stefanytam <97906503+stefanytam@users.noreply.github.com>
2022-09-21 22:02:33 -0300

    Add files via upload

---
+++ TP03/TP03/Contatos.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP03
+{
+    class Contatos
+    {
+        private readonly List<Contato> agenda;
+
+        public Contatos(List<Contato> agenda)
+        {
+            this.agenda = agenda;
+        }
+        public Contatos()
+        {
+            this.agenda = new List<Contato>();
+        }
+        public bool adicionar(Contato c)
+        {
+            agenda.Add(c);
+            return true;
+        }
+
+        public Contato pesquisar(Contato c)
+        {
+            int posicao = agenda.IndexOf(c);
+            if (posicao == -1)
+            {
+                return c;
+            }
+            return c;
+        }
+
+        public bool alterar(Contato c)
+        {
+            bool podeAlterar = false;
+            int posicao = agenda.IndexOf(c);
+            if (posicao != -1)
+            {
+                podeAlterar = true;
+            }
+            return podeAlterar;
+        }
+
+        public bool remover(Contato c)
+        {
+            return agenda.Remove(c);
+        }
+        public void listar()
+        {
+            Console.WriteLine("Qtde de coisas na lista: {0}", agenda.Count);
+            foreach (Contato c in agenda)
+            {
+                Console.WriteLine($"\n{c.ToString()}");
+            }
+            Console.WriteLine("-------------------");
+        }
+    }
+}
---
+++ TP03/TP03/Telefone.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP03
+{
+    class Telefone
+    {
+        private string tipo, numero;
+        private bool principal;
+
+        public Telefone(string tipo, string numero, bool principal)
+        {
+            this.tipo = tipo;
+            this.numero = numero;
+            this.principal = principal;
+        }
+        public Telefone()
+        {
+            this.tipo = "";
+            this.numero = "";
+            this.principal = false;
+        }
+        public Telefone(bool principal)
+        {
+            this.principal = principal;
+        }
+        public string Tipo { get => tipo; set => tipo = value; }
+        public string Numero { get => numero; set => numero = value; }
+        public bool Principal { get => principal; set => principal = value; }
+        public override bool Equals(object obj)
+        {
+            return this.principal.Equals(((Telefone)obj).principal);
+        }
+    }
+}
