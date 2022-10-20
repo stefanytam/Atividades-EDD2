9038e6b86a6d2c4870d6ac252672b027207c8e0d
stefanytam <97906503+stefanytam@users.noreply.github.com>
2022-10-20 09:50:40 -0300

    Add files via upload

---
+++ Atendimento/Atendimento/Form1.cs
@@ -0,0 +1,15 @@
+namespace Atendimento
+{
+    public partial class Atendimento : Form
+    {
+        public Atendimento()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Atendimento/Atendimento/Senhas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atendimento
+{
+    class Senhas
+    {
+        int proximoAtendimento;
+        Queue<Senha> filaSenhas;
+
+        public Senhas()
+        {
+            this.proximoAtendimento = 1;
+            this.filaSenhas = new Queue<Senha>(); 
+        }
+
+        public int ProximoAtendimento { get => proximoAtendimento; set => proximoAtendimento = value; }
+        public Queue<Senha> FilaSenhas { get => filaSenhas; set => filaSenhas = value; }
+
+        public void gerar(Senha s)
+        {
+            filaSenhas.Enqueue(s);
+        }
+
+    }
+}
