8dcd049f0208e323ec46c03f1c122fa5a853f09a
stefanytam <97906503+stefanytam@users.noreply.github.com>
2022-10-26 15:14:15 -0300

    Add files via upload

---
+++ Medicamento/Medicamento/Lote.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medicamento
+{
+    class Lote
+    {
+        public Lote(int id, int qtde, DateTime venc)
+        {
+            Id = id;
+            Qtde = qtde;
+            Venc = venc;
+        }
+        public Lote()
+        {
+            Id = 0;
+            Qtde = 0;
+            Venc = new DateTime();
+        }
+        public int Id { get; set; }
+        public int Qtde { get; set; }
+        public DateTime Venc { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id} - {Qtde} - {Venc.ToString("yyyy/MM/dd HH:mm:ss")}";
+        }
+    }
+}
---
+++ Medicamento/Medicamento/Medicamentos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medicamento
+{
+    class Medicamentos
+    {
+        public List<Medicamento> listaMedicamentos { get; }
+        public Medicamentos()
+        {
+            listaMedicamentos = new List<Medicamento>();
+        }
+
+        public void adicionar(Medicamento medicamento)
+        {
+            listaMedicamentos.Add(medicamento);
+        }
+        public bool deletar(Medicamento medicamento)
+        {
+            if (medicamento.qtdeDisponivel() == 0)
+            {
+                listaMedicamentos.Remove(medicamento);
+                return true;
+            }
+            return false;
+        }
+        public Medicamento pesquisar(Medicamento medicamento)
+        {
+            return listaMedicamentos.FirstOrDefault(m => m.Equals(medicamento));
+        }
+    }
+}
