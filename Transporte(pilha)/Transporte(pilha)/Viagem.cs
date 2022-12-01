6a5d1ab2eb1cc91036a71b67ad237ff5d496dabc
stefanytam <97906503+stefanytam@users.noreply.github.com>
2022-12-01 09:47:23 -0300

    Add files via upload

---
+++ Transporte(pilha)/Transporte(pilha)/Viagem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transporte_pilha_
+{
+     class Viagem
+    {
+        public Veiculo Veiculo { get; set; }
+        public Garagem GaragemOrigem { get; set; }
+        public Garagem GaragemDestino { get; set; }
+
+        public Viagem(Veiculo veiculo, Garagem garagemOrigem, Garagem garagemDestino)
+        {
+            Veiculo = veiculo;
+            GaragemOrigem = garagemOrigem;
+            GaragemDestino = garagemDestino;
+        }
+
+        public override string ToString()
+        {
+            string retornoFormatado = "Veiculo: " + this.Veiculo.ToString()+ "\nGaragem origem: " + this.GaragemOrigem.ToString() + "\nGaragem destino: " + this.GaragemDestino.ToString();
+
+            return retornoFormatado;
+        }
+
+
+    }
+}
